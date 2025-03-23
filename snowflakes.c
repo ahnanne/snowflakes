f198d0f49ff6771bb6a9791ebdfdd14eb9548fd6
ahnanne <54733637+ahnanne@users.noreply.github.com>
2025-03-23 11:53:02 +0900

    wip: speed -> speedY로 변경 및 속도 수정
    - 속도가 눈이 아니라 우박임 ㅋㅋ

--- snowflakes.c
+++ snowflakes.c
@@ -3,14 +3,22 @@
 #include <emscripten.h>
 
 #define NUM_SNOWFLAKES 100
+#define SPEED_Y_MIN 1
+#define SPEED_Y_MAX 20
 
 typedef struct
 {
   float x, y;
-  float speed;
+  float speedY;
 } Snowflake;
 
 Snowflake snowflakes[NUM_SNOWFLAKES];
+
+// Generate a random integer between A and B.
+int rangedRandom(int min, int max)
+{
+  return (rand() % max) + min;
+}
 
 EMSCRIPTEN_KEEPALIVE
 void init_snowflakes(int width, int height)
@@ -19,7 +27,7 @@ void init_snowflakes(int width, int height)
   {
     snowflakes[i].x = rand() % width;
     snowflakes[i].y = rand() % height;
-    snowflakes[i].speed = (rand() % 3 + 1) * 0.5;
+    snowflakes[i].speedY = rangedRandom(1, 20);
   }
 }
 
@@ -28,7 +36,7 @@ void update_snowflakes(int width, int height)
 {
   for (int i = 0; i < NUM_SNOWFLAKES; i++)
   {
-    snowflakes[i].y += snowflakes[i].speed;
+    snowflakes[i].y += snowflakes[i].speedY;
     if (snowflakes[i].y > height)
     {
       snowflakes[i].y = 0;
