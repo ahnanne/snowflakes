bb03e6c5fe11e9cbdf3752dd54911101ca75f785
ahnanne <54733637+ahnanne@users.noreply.github.com>
2025-03-29 14:00:29 +0900

    feat: wasm 사용

--- js/index.js
+++ js/index.js
@@ -1,105 +1,117 @@
-// FIXME: class
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const PARTICLES_ON_SCREEN = 245;
-const particlesArray = [];
-canvas.width = document.documentElement.clientWidth;
-canvas.height = document.documentElement.clientHeight - 10;
-let w = canvas.width;
-let h = canvas.height;
+const NUMBER_OF_SNOWFLAKES = 300;
+const SNOWFLAKE_FIELDS = 6; // 눈송이 구조체의 필드 개수
+const MARGIN = 10;
 
-function random(min, max) {
-  return min + Math.random() * (max - min + 1);
+async function loadWasm() {
+  try {
+    const response = await fetch("/snowflakes.wasm"); // WASM 파일 가져오기
+    const bytes = await response.arrayBuffer();
+    const { instance } = await WebAssembly.instantiate(bytes, {});
+
+    return instance.exports;
+  }
+  catch (error) {
+    console.error("Failed to load WASM: ", error);
+    return null;
+  }
 }
 
+// HTML 파싱 직후 wasm 받아오기
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    const wasm = await loadWasm();
+    if (wasm) {
+      window.wasmExports = wasm; // 전역 변수에 저장
+    }
+  }
+  catch (error) {
+    console.error("Failed to load WASM: ", error);
+  }
+});
+
+const mutObserver = new MutationObserver(() => {
+  if (window.wasmExports) {
+    const canvas = document.getElementById("canvas");
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+
+      const width = document.documentElement.clientWidth - MARGIN;
+      const height = document.documentElement.clientHeight - MARGIN;
+      canvas.width = width;
+      canvas.height = height;
+
+      window.wasmExports.init_snowflakes(width, height);
+
+      const snowflakes = new Float32Array( // 숫자 하나 당 4 byte
+        window.wasmExports.memory.buffer, // 버퍼(wasm가 사용하는 메모리 공간 전체) - Emscripten 기본값은 16MB(256 페이지, byte length=16908288)
+        window.wasmExports.get_snowflakes(), // 버퍼의 어느 지점부터 데이터를 읽을지 전달
+        NUMBER_OF_SNOWFLAKES * SNOWFLAKE_FIELDS, // 읽어올 요소의 개수 (눈송이 100개 * 눈송이 구조체 필드 개수 -> 총 1200 byte)
+      );
+
+      const render = () => {
+        window.wasmExports.update_snowflakes(canvas.width, canvas.height);
+
+        if (ctx) {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+
+          for (let i = 0; i < snowflakes.length; i += SNOWFLAKE_FIELDS) {
+            const gradient = ctx.createRadialGradient(
+              snowflakes[i],
+              snowflakes[i + 1],
+              0,
+              snowflakes[i],
+              snowflakes[i + 1],
+              snowflakes[i + 5],
+            );
+
+            gradient.addColorStop(
+              0,
+              `rgba(255, 255, 255, ${snowflakes[i + 4]})`
+            );
+            gradient.addColorStop(
+              0,
+              `rgba(210, 236, 242, ${snowflakes[i + 4]})`
+            );
+            gradient.addColorStop(
+              0,
+              `rgba(237, 247, 249, ${snowflakes[i + 4]})`
+            );
+
+            ctx.fillStyle = gradient;
+            ctx.beginPath();
+            ctx.arc(snowflakes[i], snowflakes[i + 1], snowflakes[i + 5], 0, Math.PI * 2);
+            ctx.fill();
+
+            ctx.shadowOffsetX = 0;
+            ctx.shadowOffsetY = 0;
+            ctx.shadowColor = "#fafafa60";
+            ctx.shadowBlur = 7;
+          }
+        }
+
+        requestAnimationFrame(render);
+        // https://developer.mozilla.org/ko/docs/Web/API/Window/requestAnimationFrame
+      };
+
+      render();
+    }
+
+    mutObserver.disconnect();
+  }
+});
+
+mutObserver.observe(document.documentElement, {
+  childList: true,
+  subtree: true,
+});
+
 function clientResize() {
-  canvas.width = document.documentElement.clientWidth;
-  canvas.height = document.documentElement.clientHeight - 10;
+  canvas.width = document.documentElement.clientWidth - MARGIN;
+  canvas.height = document.documentElement.clientHeight - MARGIN;
 
   w = canvas.width;
   h = canvas.height;
 }
 
 window.addEventListener("resize", clientResize);
-
-function createSnowFlakes() {
-  for (let i = 0; i < PARTICLES_ON_SCREEN; i++) {
-    particlesArray.push({
-      x: Math.random() * w,
-      y: Math.random() * h,
-      opacity: Math.random(),
-      speedX: random(-5, 5),
-      speedY: random(1, 20),
-      radius: random(0.5, 2.8),
-    });
-  }
-}
-
-function drawSnowFlakes() {
-  for (let i = 0; i < PARTICLES_ON_SCREEN; i++) {
-    const gradient = ctx.createRadialGradient(
-      particlesArray[i].x, // 시작 원의 x축 좌표
-      particlesArray[i].y, // 시작 원의 y축 좌표
-      0, // 시작 원의 반지름
-      particlesArray[i].x, // 끝 원의 x축 좌표
-      particlesArray[i].y, // 끝 원의 y축 좌표
-      particlesArray[i].radius // 끝 원의 반지름
-    );
-
-    gradient.addColorStop(
-      0,
-      `rgba(255, 255, 255, ${particlesArray[i].opacity})`
-    );
-    gradient.addColorStop(
-      0,
-      `rgba(210, 236, 242, ${particlesArray[i].opacity})`
-    );
-    gradient.addColorStop(
-      0,
-      `rgba(237, 247, 249, ${particlesArray[i].opacity})`
-    );
-
-    ctx.beginPath();
-    /**
-     * adds a circular arc to the current sub-path
-     * https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-     */
-    ctx.arc(
-      particlesArray[i].x, // 호의 x축 중심
-      particlesArray[i].y, // 호의 y축 중심
-      particlesArray[i].radius, // 호의 반지름
-      0, // 호 시작점 (0 radians (0°))
-      Math.PI * 2, // 호 끝점 (π = 180°, 2π = 360°)
-      false // 반시계 방향 여부 (default - false)
-    );
-
-    ctx.fillStyle = gradient;
-    ctx.fill();
-
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0;
-    ctx.shadowColor = "#fafafa60";
-    ctx.shadowBlur = 7;
-  }
-}
-
-function moveSnowFlakes() {
-  for (let i = 0; i < particlesArray.length; i++) {
-    particlesArray[i].x += particlesArray[i].speedX;
-    particlesArray[i].y += particlesArray[i].speedY;
-
-    if (particlesArray[i].y > h) {
-      particlesArray[i].x = Math.random() * w * 1.5;
-      particlesArray[i].y = -50;
-    }
-  }
-}
-
-function updateSnowFall() {
-  ctx.clearRect(0, 0, w, h);
-  drawSnowFlakes();
-  moveSnowFlakes();
-}
-
-window.setInterval(updateSnowFall, 50);
-createSnowFlakes();
